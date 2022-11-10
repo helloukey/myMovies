f4e933a9efc4540932cc8db22f60c107b7298e0b
helloukey <kunalukey32@gmail.com>
2022-11-10 23:06:01 +0530

    updated fetch url on client side.

--- client/src/redux/tmdbApi.js
+++ client/src/redux/tmdbApi.js
@@ -2,17 +2,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const tmdbApi = createApi({
   reducerPath: "tmdbApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://mymovies.onrender.com/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
   endpoints: (builder) => ({
     getTrending: builder.query({
       query: (type) =>
-        `api/trending/${type}`,
+        `/trending/${type}`,
     }),
 
     getSingleTrending: builder.query({
       async queryFn(type, _queryApi, _extraOptions, fetchWithBQ) {
         const trendingList = await fetchWithBQ(
-          `api/trending/${type}`
+          `/trending/${type}`
         );
         if (trendingList.error) return { error: trendingList.error };
         const singleData = await trendingList.data.results.filter(
@@ -20,13 +20,13 @@ export const tmdbApi = createApi({
         )[Math.floor(Math.random() * 10)];
         if (singleData.media_type === "movie") {
           const result = await fetchWithBQ(
-            `api/trending/movie/${singleData.id}`
+            `/trending/movie/${singleData.id}`
           );
           return result.data ? { data: {...result.data, mediaType: "movie"} } : { error: result.error };
         }
         if (singleData.media_type === "tv") {
           const result = await fetchWithBQ(
-            `api/trending/show/${singleData.id}`
+            `/trending/show/${singleData.id}`
           );
           return result.data ? { data: {...result.data, mediaType: "show"} } : { error: result.error };
         }
@@ -35,49 +35,49 @@ export const tmdbApi = createApi({
 
     getMovieList: builder.query({
       query: (type) =>
-        `api/list/movie/${type}`,
+        `/list/movie/${type}`,
     }),
 
     getShowList: builder.query({
       query: (type) =>
-        `api/list/show/${type}`,
+        `/list/show/${type}`,
     }),
 
     getMovieTrendingPage: builder.query({
       query: (page = 1) =>
-        `api/trending/movie/page/${page}`,
+        `/trending/movie/page/${page}`,
     }),
 
     getMovieNowPlayingPage: builder.query({
       query: (page = 1) =>
-        `api/movie/nowplaying/${page}`,
+        `/movie/nowplaying/${page}`,
     }),
 
     getMoviePopularPage: builder.query({
       query: (page = 1) =>
-        `api/movie/popular/${page}`,
+        `/movie/popular/${page}`,
     }),
 
     getMovieTopRatedPage: builder.query({
       query: (page = 1) =>
-        `api/movie/toprated/${page}`,
+        `/movie/toprated/${page}`,
     }),
 
     getMovieUpcomingPage: builder.query({
       query: (page = 1) =>
-        `api/movie/upcoming/${page}`,
+        `/movie/upcoming/${page}`,
     }),
 
     getMovieDetailsPage: builder.query({
       query: (movie_id) =>
-        `api/details/movie/${movie_id}`,
+        `/details/movie/${movie_id}`,
     }),
 
     getMovieGenrePage: builder.query({
       query: (args) => { 
         const {page, genre} = args;
         return {
-          url: `api/movie/genre/${genre}/${page}`,
+          url: `/movie/genre/${genre}/${page}`,
           params: { page, genre }
         }
       }
@@ -85,39 +85,39 @@ export const tmdbApi = createApi({
 
     getShowTrendingPage: builder.query({
       query: (page = 1) =>
-        `api/trending/show/page/${page}`,
+        `/trending/show/page/${page}`,
     }),
 
     getShowAiringTodayPage: builder.query({
       query: (page = 1) =>
-        `api/show/airingtoday/${page}`,
+        `/show/airingtoday/${page}`,
     }),
 
     getShowPopularPage: builder.query({
       query: (page = 1) =>
-        `api/show/popular/${page}`,
+        `/show/popular/${page}`,
     }),
 
     getShowTopRatedPage: builder.query({
       query: (page = 1) =>
-        `api/show/toprated/${page}`,
+        `/show/toprated/${page}`,
     }),
 
     getShowOnTheAirPage: builder.query({
       query: (page = 1) =>
-        `api/show/ontheair/${page}`,
+        `/show/ontheair/${page}`,
     }),
 
     getShowDetailsPage: builder.query({
       query: (show_id) =>
-        `api/details/show/${show_id}`,
+        `/details/show/${show_id}`,
     }),
 
     getShowEpisodes: builder.query({
       query: (args) => { 
         const {show_id, seasonNumber} = args;
         return {
-          url: `api/episodes/show/${show_id}/${seasonNumber}`,
+          url: `/episodes/show/${show_id}/${seasonNumber}`,
           params: { show_id, seasonNumber }
         }
       }
@@ -127,21 +127,21 @@ export const tmdbApi = createApi({
       query: (args) => { 
         const {page, genre} = args;
         return {
-          url: `api/show/genre/${genre}/${page}`,
+          url: `/show/genre/${genre}/${page}`,
           params: { page, genre }
         }
       }
     }),
 
     getPersonDetailsPage: builder.query({
-      query: (person_id) => `api/person/${person_id}`
+      query: (person_id) => `/person/${person_id}`
     }),
 
     getSearchResults: builder.query({
       query: (args) => { 
         const {query, page} = args;
         return {
-          url: `api/search/${query}/${page}`,
+          url: `/search/${query}/${page}`,
           params: { query, page }
         }
       }
