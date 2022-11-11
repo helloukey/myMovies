bdb802538a054dbc418364569cc068385c836ab9
helloukey <kunalukey32@gmail.com>
2022-11-11 16:05:48 +0530

    added proxy to tmdb api.

--- client/src/redux/tmdbApi.js
+++ client/src/redux/tmdbApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const tmdbApi = createApi({
   reducerPath: "tmdbApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://mymovies.onrender.com/api" }),
   endpoints: (builder) => ({
     getTrending: builder.query({
       query: (type) =>
