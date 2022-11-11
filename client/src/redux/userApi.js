67c1eae8ae0dbd294f7609740bf499e4e3d19346
helloukey <kunalukey32@gmail.com>
2022-11-11 16:34:34 +0530

    changed base url on client side.

--- client/src/redux/userApi.js
+++ client/src/redux/userApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/",
+    baseUrl: "http://mymovies.onrender.com/",
   }),
   tagTypes: ["collection"],
   endpoints: (builder) => ({
