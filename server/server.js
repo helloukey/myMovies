ac52d5b9e618a7fb4c01bebba46c0fcd72ef87d7
helloukey <kunalukey32@gmail.com>
2022-11-11 07:35:25 +0530

    added client build path and route to server.js file.

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ const userRoutes = require("./routes/user");
 const collectionRoutes = require("./routes/collection");
 const userAuthorization = require("./middleware/userAuthorization");
 
+// use client side build
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -35,3 +40,8 @@ app.use("/user", userAuthorization, userRoutes);
 // autherizatin middleware
 // app.use(userAuthorization);
 app.use("/user", collectionRoutes);
+
+// only when ready to deploy
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
