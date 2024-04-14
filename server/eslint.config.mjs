5b73fcfbe20c2e577a71ff6b4c663e0fc0e0c4df
Kunal <kunalukey32@gmail.com>
2024-04-14 22:54:45 +0530

    added eslint config for server

---
+++ server/eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+
+export default [
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  ...compat.extends("prettier"),
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+    },
+  },
+  {
+    ignores: [
+      "node_modules",
+      ".env",
+      ".env_sample",
+      ".prettierignore",
+      ".prettierrc",
+    ],
+  },
+  eslintConfigPrettier,
+];
