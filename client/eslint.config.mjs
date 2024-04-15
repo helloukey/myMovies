992b007ee97702e6a24efbb2adda50304ba248d8
Kunal <kunalukey32@gmail.com>
2024-04-15 11:21:11 +0530

    added husky config for client and server

---
+++ client/eslint.config.mjs
@@ -0,0 +1,53 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+  ...compat.extends("prettier"),
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+  {
+    ignores: [
+      ".vscode",
+      "cypress",
+      "node_modules",
+      "public",
+      "src/assets",
+      ".prettierrc",
+      ".prettierignore",
+      "cypress.config.js",
+      "eslint.config.mjs",
+    ],
+  },
+  eslintConfigPrettier,
+];
